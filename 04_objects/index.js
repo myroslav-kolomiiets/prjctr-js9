349cb68ceb78cfae823a84e72a59bfeba00f630a
Miroslav <miroslav@Miroslavs-MacBook-Pro.local>
2023-10-03 21:13:57 +0300

    Object in details webinar files

---
+++ 04_objects/index.js
@@ -0,0 +1,257 @@
+'use strict';
+
+/**
+ * Об'єкти в деталях
+ * @author Miroslav
+ */
+
+let id = Symbol("unique person id");
+// let anotherId = Symbol("unique person id");
+// console.log(id === anotherId);
+
+const person = {
+    name: 'Anton',
+    age: 56,
+    mainRole: 'admin',
+    "secondary role": 'admin',
+    isOnline: false,
+    roles: ["user", "support", "admin"],
+    sayHi: () => {
+        console.log("Hello there!");
+    },
+    params: {
+        height: 175,
+        weight: 85,
+    },
+    [id]: 42,
+};
+
+// console.log(person.name);
+
+// const personClone = person;
+// personClone.name = "Anna";
+// console.log(person);
+
+// person.id = 555;
+// console.log(person);
+
+// let someKey = 'mainRole';
+// console.log(person[someKey])
+
+/**
+ * CRUD операції
+ */
+
+// create
+// const initObject = {name: 'someName'};
+// const secondObject = new Object({});
+
+// read
+// console.log(initObject)
+// console.log(secondObject)
+
+// const key = 'isOnline';
+// console.log(person[key]);
+
+// update
+// person.name = "Anna";
+// person.test = 'test';
+// console.log(person);
+
+// delete
+// delete person.name;
+// console.log(person);
+
+/**
+ * Перевірка існування ключів
+ */
+
+// let isThereSomeHeight = 'height' in person.params;
+// console.log(isThereSomeHeight);
+
+/**
+ * Робота з циклом
+ */
+
+// for (let key in person) {
+// 	console.log(person[key]);
+// }
+
+/**
+ * Опційний ланцюжок
+ */
+
+// const personColor = person?.params?.color || "some default value";
+// const personHeight = person?.params?.height || "some default value";
+// console.log(personColor); // false
+// console.log(personHeight); // true
+
+/**
+ * Object.keys, Object.values
+ */
+
+// console.log(Object.keys(person));
+// console.log(Object.values(person));
+
+/**
+ * Object.entries, Object.fromEntries
+ */
+
+// console.log(Object.entries(person));
+
+// let capsPerson = Object.fromEntries(
+//   Object.entries(person).map(([key, value]) => [key.toUpperCase(), value])
+// );
+
+// console.log(capsPerson);
+
+// capsPerson.SAYHI();
+
+/**
+ * Примітивний тип символ
+ */
+
+// console.log(person.id);
+// console.log(person[id]);
+
+/**
+ * Про що варто пам'ятати?
+ */
+
+/**
+ * null теж об'єкт
+ */
+
+// console.log(typeof null);
+
+// if (
+// 	typeof person === 'object' &&
+// 	person !== null &&
+// 	!Array.isArray(person)
+// ) {
+// 	console.log('its object');
+// }
+
+/**
+ * Копіювання об'єктів через Object.assign (для першого рівня)
+ */
+
+// const personClone = Object.assign({}, person);
+//
+// console.log(personClone);
+
+/**
+ * Глибоке копіювання об'єктів
+ */
+
+// let anotherPerson = JSON.parse(JSON.stringify(person));
+//
+// anotherPerson.name = 'Oleg';
+//
+// anotherPerson.params.height = 180;
+//
+// console.log(person);
+// console.log(anotherPerson);
+
+/**
+ * Як зробити об'єкт незмінним
+ */
+
+// Object.freeze(person);
+//
+// person.name = 'Nataly';
+//
+// console.log(person);
+
+/**
+ * Вбудований об'єкт Date
+ */
+
+// const date1 = new Date("2022-01-10");
+// console.log(date1);
+//
+// const date2 = new Date(0);
+// console.log(date2);
+//
+// const date3 = new Date(2022, 1, 10);
+// console.log(date3);
+//
+// const date4 = new Date();
+// console.log(date4);
+//
+// console.log(date4.getFullYear());
+// console.log(date4.getDay());
+
+/**
+ * Date.now()
+ */
+
+// const start = Date.now();
+// for (let i = 0; i < 1000000000; i++) {
+// 	++person[id]
+// }
+// console.log(`Time elapsed: ${Date.now() - start} ms`);
+
+/**
+ * performance.now()
+ */
+
+// let start = performance.now();
+// for (let i = 0; i < 1000000000; i++) {
+// 	++person[id]
+// }
+// let end = performance.now();
+//
+// console.log(`Time elapsed: ${end - start} ms`);
+
+/**
+ * Date.parse()
+ */
+
+// console.log(Date.parse('03 Aug 1985'));
+
+/**
+ * деструктуризація масивів
+ */
+
+// const users = ["Igor", "Olga", "Petro", "Anna", "Olga"];
+//
+// const user1 = users[0];
+// const user2 = users[1];
+// const user3 = users[2];
+//
+// let [user1, user2, user3] = users;
+//
+// console.log(user1)
+// console.log(user2)
+// console.log(user3)
+
+/**
+ * деструктуризація об'єктів
+ */
+
+// let person2 = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+// };
+
+// let firstName = person2.firstName;
+// let lastName = person2.lastName;
+//
+// let { firstName, lastName } = person2;
+
+// console.log(firstName);
+// console.log(lastName);
+
+/**
+ * spread синтаксис
+ */
+
+const array = [1, 2, 3];
+const obj = { ...array }; // { 0: 1, 1: 2, 2: 3 }
+console.log(obj);
+
+// const array1 = [1, 2, 3, 4, 5];
+// const array2 = [6, 7, 8, 9, 10];
+// const array3 = [...array1, ...array2];
+// console.log(array3);
---
+++ 04_objects/tasks.js
@@ -0,0 +1,42 @@
+/**
+ * Задача №1
+ * Що буде в масиві users?
+ */
+
+// const users = ["Igor", "Olga", "Petro", "Anna"];
+// const copyOfUsers = users;
+//
+// copyOfUsers.push('Ivan');
+//
+// console.log(users);
+
+/**
+ * Задача №2
+ * Який буде результат приведення типів?
+ */
+
+// console.log([] + null + 1); // 'null1'
+// console.log(['text'] + null + 1); // 'textnull1'
+// console.log(10 + null + 1);
+// console.log([10, 11, 12] + null + 1); // '10,11,12null1'
+// console.log([1,2,3] === [1,2,3]); // false
+// console.log([1,2,3] == [1,2,3]);
+// console.log([] && false);
+// console.log([] || false);
+// console.log(['test1'] + ['test2']);
+
+
+// const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович",
+// "Рудко Андрій Опанасович"];
+//
+// let initials = [];
+// const str = "Петрик Ольга Іванівна";
+// for (let i=0; i<userNames.length; i++){
+//     for (let j = 0; j < userNames[i].length; j++) {
+//         if (userNames[i][j].match(/[А-ЩЬЮЯҐЄІЇ]/)) {
+//             initials[i]+=userNames[i][j]+'.';
+//         }
+//     }
+// }
+//
+// console.log(initials);
