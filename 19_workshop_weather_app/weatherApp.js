bc99ba006379e4b23e0ef385dbc6eeb53b8a36b4
Miroslav <miroslav@Miroslavs-MacBook-Pro.local>
2023-12-05 21:03:35 +0200

    Workshop files

---
+++ 19_workshop_weather_app/weatherApp.js
@@ -0,0 +1,44 @@
+// const url = `https://api.openweathermap.org/data/2.5/weather?q=SEARCH_VALUE&appid=API_KEY&units=metric`;
+// const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`;
+
+import {input, list} from './constants.js';
+
+export class WeatherApp {
+    constructor(API_KEY) {
+        this.API_KEY = API_KEY;
+        this.isLoading = false;
+    }
+
+    fetchData = () => {
+        const inputVal = input.value;
+        const url =
+            `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${this.API_KEY}&units=metric`;
+
+        this.isLoading = true;
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                this.updateDOM(data);
+            })
+            .catch((error) => {
+                throw new Error(error);
+            })
+            .finally(() => {
+                this.isLoading = false;
+            })
+    }
+
+    updateDOM = (data) => {
+        const {main, name, sys, weather} = data;
+
+        const li = document.createElement('li');
+        li.classList.add('city');
+
+        li.innerHTML = `<h2 class="city-name">${name}</h2>
+                        <div class="city-temp">${main.temp}</div>
+                        `;
+
+        list.appendChild(li);
+    }
+}
