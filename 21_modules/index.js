f84ab50fff32197ec8e424fbe8cddd2c1a319489
Miroslav <miroslav@Miroslavs-MacBook-Pro.local>
2023-12-13 09:41:26 +0200

    Webinar files

---
+++ 21_modules/index.js
@@ -0,0 +1,21 @@
+/**
+ * Modules 1
+ * @author Miroslav
+ */
+
+// 24. Модулі та import/export 
+// import {sum as sum2, user} from './module1.js'
+
+import SomeFunc from './module1.js';
+
+const sum = () => {
+
+};
+
+// const user ={
+//     name: 'Petro',
+// }
+
+// console.log(sum2(2, 45));
+//
+console.log(SomeFunc);
---
+++ 21_modules/module1.js
@@ -0,0 +1,19 @@
+export function sum (a, b) {
+    return hiddenFunctionSum(a, b);
+}
+
+export function hiddenFunctionSum(a, b) {
+    return a + b;
+}
+
+export const user = {
+    name: 'Yurko',
+}
+
+export default function add() {
+
+}
+
+console.log('hello!');
+
+// export { sum, user };
